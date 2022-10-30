da35f4bae7dfbea007c734fc921573762a3c5d73
Bren Briggs <code@fraq.io>
2022-10-30 18:19:48 -0400

    Pretty print stack output

---
+++ cmd/getStack.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+)
+
+var stackName string
+
+// stackCmd represents the stack command
+var getStackCmd = &cobra.Command{
+	Use:   "stack",
+	Short: "Get information about a bytebot stack",
+	Run: func(cmd *cobra.Command, args []string) {
+		name := viper.GetString("name")
+		for _, stack := range C.Stacks {
+			if stack.Name == name {
+				prettyYaml, err := yaml.Marshal(&stack)
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Printf("---\n%s\n\n", string(prettyYaml))
+			}
+		}
+	},
+}
+
+func init() {
+	getCmd.AddCommand(getStackCmd)
+	getStackCmd.Flags().StringVarP(&stackName, "name", "n", "", "Name of bytebot stack")
+	viper.BindPFlag("name", getStackCmd.Flags().Lookup("name"))
+}
