e359145322b36fb0705a2255c317a60f8c21fbea
Bren Briggs <code@fraq.io>
2022-11-04 00:58:12 +0000

    Generate redis config as part of service stack

--- ctl/runDockerCompose.go
+++ ctl/runDockerCompose.go
@@ -9,6 +9,7 @@ import (
 const COMPOSE_VERSION = "3.8"
 const REDIS_IMAGE = "redis"
 const REDIS_IMAGE_TAG = "6.2.3"
+const REDIS_DEFAULT_PORT = "6379"
 
 type ComposeFile struct {
 	Version  string                    `yaml:"version"`
@@ -23,20 +24,25 @@ type ComposeService struct {
 	ContainerName string   `yaml:"container_name"`
 	Image         string   `yaml:"image"`
 	Build         string   `yaml:"build,omitempty"`
-	Command       []string `yaml:"command"`
+	Command       []string `yaml:"command,omitempty"`
 	DependsOn     []string `yaml:"depends_on,omitempty"`
 	EnvFile       []string `yaml:"env_file,omitempty"`
 }
 
 func NewDockerComposeConfig(stack Stack) (*ComposeFile, error) {
-	config := new(ComposeFile)
-	config.Services = make(map[string]ComposeService)
-	prefix := stack.Name
+	var (
+		config    *ComposeFile = new(ComposeFile)
+		prefix    string       = stack.Name
+		redisAddr string       = stack.RedisAddr // If redisAddr is set and manageRedis is true, we want to override redisAddr
+	)
 	config.Version = COMPOSE_VERSION
+	config.Services = make(map[string]ComposeService) // I always forget that maps have to be initialized
 
 	if stack.ManageRedis {
+		redisSvcName := fmt.Sprintf("%s-%s", prefix, "redis")
+		redisAddr = fmt.Sprintf("%s:%s", redisSvcName, REDIS_DEFAULT_PORT)
 		redisSvc := ComposeService{
-			ContainerName: fmt.Sprintf("%s-%s", prefix, "redis"),
+			ContainerName: redisSvcName,
 			Image:         fmt.Sprintf("%s:%s", REDIS_IMAGE, REDIS_IMAGE_TAG),
 		}
 		config.Services[fmt.Sprintf("%s-redis", prefix)] = redisSvc
@@ -54,7 +60,7 @@ func NewDockerComposeConfig(stack Stack) (*ComposeFile, error) {
 		}
 
 		command = append(command, "-redis")
-		command = append(command, fmt.Sprintf(stack.RedisAddr))
+		command = append(command, redisAddr)
 		command = append(command, svc.ExtraArgs...)
 
 		composeSvc := ComposeService{
